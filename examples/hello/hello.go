04a8537c99b911ced32e7516bf321c0d51cb27bb
Lucas Stadler <luc@spreadshirt.net>
2017-08-17 14:05:28 +0200

    Show how to use ratio flags in the example

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -3,18 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/spreadshirt/go-feature"
 )
 
 var features struct {
-	Scream feature.Flag
+	Scream   feature.Flag
+	Surprise feature.Flag
 }
 
 func main() {
+	// initialize RNG so that ratio flags are actually random
+	rand.Seed(time.Now().UnixNano())
+
 	featureSet := feature.NewSet()
 	features.Scream, _ = featureSet.NewFlag("scream")
+	features.Surprise = feature.NewRatioFlag("surprise", 0.1)
+	featureSet.Add(features.Surprise)
 
 	http.HandleFunc("/hello", helloHandler)
 	http.Handle("/features/", featureSet)
@@ -29,6 +37,9 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 	} else {
 		msg = "hello."
 	}
-
 	fmt.Fprintf(w, msg)
+
+	if features.Surprise.IsEnabled() {
+		fmt.Fprintf(w, "\n\nsurpriiiise!!!")
+	}
 }
